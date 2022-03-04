77498fa243a1299684cfcd141705d6ba34f5762f
fabiohild <fabiohildebrand@gmail.com>
2022-03-04 17:50:23 -0300

    Minimal treasury for community gov

--- src/GebDaoMinimalTreasury.sol
+++ src/GebDaoMinimalTreasury.sol
@@ -1,5 +1,45 @@
 // SPDX-License-Identifier: GPL-3.0-or-later
-pragma solidity ^0.8.6;
+pragma solidity 0.6.7;
 
-contract GebDaoMinimalTreasury {
+import "./auth/GebAuth.sol";
+
+abstract contract TokenLike {
+    function transfer(address, uint256) external virtual;
+}
+
+// @notice Minimal treasury for the community DAO
+contract GebDaoMinimalTreasury is GebAuth {
+
+    /**
+     * @notice Transfer tokens from treasury to dst
+     * @param token The address to transfer tokens from
+     * @param dst The address to transfer tokens to
+     * @param amount The amount of tokens to transfer
+     */
+    function transferERC20(address token, address dst, uint256 amount) external isAuthorized {
+        TokenLike(token).transfer(dst, amount);
+    }
+
+    /**
+     * @notice Transfer ether from treasury to dst
+     * @param dst The address to transfer tokens to
+     * @param value The ETH value to transfer
+     */
+    function transferEther(address dst, uint256 value) external isAuthorized {
+        (bool success, ) = dst.call{value: value}("");
+        require(success);
+    }
+
+    /**
+     * @notice Perform arbitrary calls from the treasury
+     * @param target The address to send the call to
+     * @param value The ETH value to transfer
+     * @param data Input raw data for the call
+     */
+    function externalCall(address target, uint256 value, bytes calldata data) external isAuthorized {
+        (bool success, ) = target.call{value: value}(data);
+        require(success);
+    }
+
+    receive() external payable {}
 }
--- src/GebDaoMinimalTreasury.t.sol
+++ src/GebDaoMinimalTreasury.t.sol
@@ -1,9 +1,17 @@
 // SPDX-License-Identifier: GPL-3.0-or-later
-pragma solidity ^0.8.6;
+pragma solidity 0.6.7;
 
 import "ds-test/test.sol";
+import "ds-token/token.sol";
 
 import "./GebDaoMinimalTreasury.sol";
+
+contract Target {
+    uint256 public calls;
+    function arbitraryCall() external payable {
+        calls++;
+    }
+}
 
 contract GebDaoMinimalTreasuryTest is DSTest {
     GebDaoMinimalTreasury treasury;
@@ -12,11 +20,63 @@ contract GebDaoMinimalTreasuryTest is DSTest {
         treasury = new GebDaoMinimalTreasury();
     }
 
-    function testFail_basic_sanity() public {
-        assertTrue(false);
+    modifier removeAuth() {
+        treasury.removeAuthorization(address(this));
+        _;
     }
 
-    function test_basic_sanity() public {
-        assertTrue(true);
+    function testTransferERC20() external {
+        DSToken token = new DSToken("name", "symbol");
+        token.mint(address(treasury), 1000 ether);
+        assertEq(token.balanceOf(address(treasury)), 1000 ether);
+        treasury.transferERC20(address(token), address(0xfab), 1000 ether);
+        assertEq(token.balanceOf(address(treasury)), 0);
+        assertEq(token.balanceOf(address(0xfab)), 1000 ether);
+    }
+
+    function testFailTransferERC20Unauthorized() external removeAuth {
+        DSToken token = new DSToken("name", "symbol");
+        token.mint(address(treasury), 1000 ether);
+        assertEq(token.balanceOf(address(treasury)), 1000 ether);
+        treasury.transferERC20(address(token), address(0xfab), 1000 ether);
+    }
+
+    function testTransferEther() external {
+        address(treasury).transfer(100 ether);
+        assertEq(address(treasury).balance, 100 ether);
+        treasury.transferEther(address(0xfab), 100 ether);
+        assertEq(address(treasury).balance, 0);
+        assertEq(address(0xfab).balance, 100 ether);
+    }
+
+    function testFailTransferEtherUnauthorized() external removeAuth {
+        address(treasury).transfer(100 ether);
+        assertEq(address(treasury).balance, 100 ether);
+        treasury.transferEther(address(0xfab), 100 ether);
+    }
+
+    function testExternalCall() external {
+        Target target = new Target();
+        assertEq(target.calls(), 0);
+        address(treasury).transfer(100 ether);
+
+        treasury.externalCall(
+            address(target),
+            100 ether,
+            abi.encodePacked(Target.arbitraryCall.selector)
+        );
+        assertEq(target.calls(), 1);
+    }
+
+    function testFailExternalCallUnauthorized() external removeAuth {
+        Target target = new Target();
+        assertEq(target.calls(), 0);
+        address(treasury).transfer(100 ether);
+
+        treasury.externalCall(
+            address(target),
+            100 ether,
+            abi.encodePacked(Target.arbitraryCall.selector)
+        );
     }
 }
