0d318b40d18259e2e162e361fcfabfc9c3d136b8
fabiohild <fabiohildebrand@gmail.com>
2022-03-08 10:08:57 -0300

    add delegate

--- src/GebDaoMinimalTreasury.sol
+++ src/GebDaoMinimalTreasury.sol
@@ -7,39 +7,120 @@ abstract contract TokenLike {
     function transfer(address, uint256) external virtual;
 }
 
-// @notice Minimal treasury for the community DAO
+/**
+* @notice   Minimal treasury for the community DAO
+*           Allows for delegating control of the treasury (fixed amount per epoch)
+*           Governance can update allowance or revoke rights at any time
+*           Increases in allowance take effect only in next epoch, decreases immediately
+**/
 contract GebDaoMinimalTreasury is GebAuth {
+    // --- State vars ---
+    // Token kept in the treasury
+    TokenLike immutable public token;
+    // delegate, can spend allowance every epoch
+    address public treasuryDelegate;
+    // duration of each epoch (seconds)
+    uint256 public epochLength;
+    // amount that can be spent each epoch
+    uint256 public delegateAllowance;
+    // amount left to spend in current epock
+    uint256 public delegateLeftoverToSpend;
+    // current epoch start (Unix timestamp)
+    uint256 public epochStart;
+
+    // --- Constructor ---
+    /**
+     * @notice Constructor
+     * @param _token Token to be used
+     * @param _delegate Delegate
+     * @param _epochLength Duration of each epoch (seconds)
+     * @param _delegateAllowance Amount that can be spent by the delegate each epoch
+     */
+    constructor(
+        address _token,
+        address _delegate,
+        uint256 _epochLength,
+        uint256 _delegateAllowance
+    ) public {
+        require(_epochLength > 0, "GebDAOMinimalTreasury/invalid-epoch");
+        require(_token != address(0), "GebDAOMinimalTreasury/invalid-epoch");
+        token = TokenLike(_token);
+        treasuryDelegate = _delegate;
+        epochLength = _epochLength;
+        delegateAllowance = _delegateAllowance;
+        epochStart = now;
+        delegateLeftoverToSpend = _delegateAllowance;
+    }
+    // --- SafeMath ---
+    function subtract(uint256 x, uint256 y) internal pure returns (uint256 z) {
+        require((z = x - y) <= x, "MultiAccountingEngine/sub-underflow");
+    }
+
+    // --- Boolean Logic ---
+    function either(bool x, bool y) internal pure returns (bool z) {
+        assembly{ z := or(x, y)}
+    }
+
+    // --- Admin functions ---
+    /**
+    * @notice Modify an int256 parameter
+    * @param parameter The name of the parameter to change
+    * @param val The new value for the parameter
+    **/
+    function modifyParameters(bytes32 parameter, uint256 val) external isAuthorized {
+        if (parameter == "epochLength") {
+          require(val > 0, "GebDAOMinimalTreasury/invalid-epochLength");
+          epochLength = val;
+        }
+        else if (parameter == "delegateAllowance") {
+          delegateAllowance = val;
+          if (val < delegateLeftoverToSpend)
+            delegateLeftoverToSpend = val;
+        }
+        else revert("GebDAOMinimalTreasury/modify-unrecognized-param");
+    }
+
+    /**
+    * @notice Modify an int256 parameter
+    * @param parameter The name of the parameter to change
+    * @param val The new value for the parameter
+    **/
+    function modifyParameters(bytes32 parameter, address val) external isAuthorized {
+        if (parameter == "treasuryDelegate") {
+          treasuryDelegate = val;
+        }
+        else revert("GebDAOMinimalTreasury/modify-unrecognized-param");
+    }
+
+    // --- Delegate functions ---
+
+    /**
+    * @notice Updates epoch info.
+    *         Balance not used in previous epochs should not be available
+    **/
+    modifier updateEpoch() {
+        uint256 epochFinish = epochStart + epochLength;
+        if (now > epochFinish) {
+            delegateLeftoverToSpend = delegateAllowance;
+            if (now - epochFinish > epochLength) {
+                uint256 epochsElapsed = (now - epochFinish) / epochLength;
+                epochStart = (epochsElapsed * epochLength) + epochFinish;
+            } else
+                epochStart = epochFinish;
+        }
+        _;
+    }
 
     /**
      * @notice Transfer tokens from treasury to dst
-     * @param token The address to transfer tokens from
      * @param dst The address to transfer tokens to
      * @param amount The amount of tokens to transfer
      */
-    function transferERC20(address token, address dst, uint256 amount) external isAuthorized {
-        TokenLike(token).transfer(dst, amount);
+    function transferERC20(address dst, uint256 amount) external updateEpoch {
+        require(msg.sender == treasuryDelegate || authorizedAccounts[msg.sender] == 1, "GebDAOMinimalTreasury/unauthorized");
+        if (msg.sender == treasuryDelegate) {
+            delegateLeftoverToSpend = subtract(delegateLeftoverToSpend, amount); // reverts if lower allowance
+        }
+        token.transfer(dst, amount);
     }
-
-    /**
-     * @notice Transfer ether from treasury to dst
-     * @param dst The address to transfer tokens to
-     * @param value The ETH value to transfer
-     */
-    function transferEther(address dst, uint256 value) external isAuthorized {
-        (bool success, ) = dst.call{value: value}("");
-        require(success);
-    }
-
-    /**
-     * @notice Perform arbitrary calls from the treasury
-     * @param target The address to send the call to
-     * @param value The ETH value to transfer
-     * @param data Input raw data for the call
-     */
-    function externalCall(address target, uint256 value, bytes calldata data) external isAuthorized {
-        (bool success, ) = target.call{value: value}(data);
-        require(success);
-    }
-
-    receive() external payable {}
 }
--- src/GebDaoMinimalTreasury.t.sol
+++ src/GebDaoMinimalTreasury.t.sol
@@ -6,18 +6,97 @@ import "ds-token/token.sol";
 
 import "./GebDaoMinimalTreasury.sol";
 
-contract Target {
-    uint256 public calls;
-    function arbitraryCall() external payable {
-        calls++;
+abstract contract Hevm {
+    function warp(uint) virtual public;
+    function roll(uint) virtual public;
+}
+
+contract Delegate {
+    function transferERC20(GebDaoMinimalTreasury treasury, address dst, uint amount) public {
+        treasury.transferERC20(dst, amount);
     }
 }
 
 contract GebDaoMinimalTreasuryTest is DSTest {
     GebDaoMinimalTreasury treasury;
+    Delegate delegate;
+    DSToken token;
+    uint256 epochLength = 4 weeks;
+    uint256 allowance   = 100 ether;
+    uint256 initialTreasuryBalance = 1e6 ether;
+
+    Hevm hevm = Hevm(0x7109709ECfa91a80626fF3989D68f67F5b1DD12D);
 
     function setUp() public {
-        treasury = new GebDaoMinimalTreasury();
+        delegate = new Delegate();
+        token = new DSToken("name", "symbol");
+        treasury = new GebDaoMinimalTreasury(
+            address(token),
+            address(delegate),
+            epochLength,
+            allowance
+        );
+
+        token.mint(address(treasury), initialTreasuryBalance);
+    }
+
+    function test_constructor() public {
+        assertEq(address(treasury.token()), address(token));
+        assertEq(address(treasury.treasuryDelegate()), address(delegate));
+        assertEq(treasury.epochLength(), epochLength);
+        assertEq(treasury.delegateAllowance(), allowance);
+        assertEq(treasury.epochStart(), now);
+        assertEq(treasury.delegateLeftoverToSpend(), allowance);
+    }
+
+    function test_constructor_no_delegate() public {
+        treasury = new GebDaoMinimalTreasury(
+            address(token),
+            address(0),
+            epochLength,
+            allowance
+        );
+
+        assertEq(address(treasury.token()), address(token));
+        assertEq(address(treasury.treasuryDelegate()), address(0));
+        assertEq(treasury.epochLength(), epochLength);
+        assertEq(treasury.delegateAllowance(), allowance);
+        assertEq(treasury.epochStart(), now);
+        assertEq(treasury.delegateLeftoverToSpend(), allowance);
+    }
+
+    function test_constructor_no_allowance() public {
+        treasury = new GebDaoMinimalTreasury(
+            address(token),
+            address(delegate),
+            epochLength,
+            0
+        );
+
+        assertEq(address(treasury.token()), address(token));
+        assertEq(address(treasury.treasuryDelegate()), address(delegate));
+        assertEq(treasury.epochLength(), epochLength);
+        assertEq(treasury.delegateAllowance(), 0);
+        assertEq(treasury.epochStart(), now);
+        assertEq(treasury.delegateLeftoverToSpend(), 0);
+    }
+
+    function testFail_constructor_invalid_token() public {
+        treasury = new GebDaoMinimalTreasury(
+            address(0),
+            address(delegate),
+            epochLength,
+            allowance
+        );
+    }
+
+    function testFail_constructor_invalid_epoch_length() public {
+        treasury = new GebDaoMinimalTreasury(
+            address(token),
+            address(delegate),
+            0,
+            allowance
+        );
     }
 
     modifier removeAuth() {
@@ -25,58 +104,88 @@ contract GebDaoMinimalTreasuryTest is DSTest {
         _;
     }
 
-    function testTransferERC20() external {
-        DSToken token = new DSToken("name", "symbol");
-        token.mint(address(treasury), 1000 ether);
-        assertEq(token.balanceOf(address(treasury)), 1000 ether);
-        treasury.transferERC20(address(token), address(0xfab), 1000 ether);
-        assertEq(token.balanceOf(address(treasury)), 0);
-        assertEq(token.balanceOf(address(0xfab)), 1000 ether);
+    function test_modify_parameters_epochLength() public {
+        treasury.modifyParameters("epochLength", 1 weeks);
+        assertEq(treasury.epochLength(), 1 weeks);
     }
 
-    function testFailTransferERC20Unauthorized() external removeAuth {
-        DSToken token = new DSToken("name", "symbol");
-        token.mint(address(treasury), 1000 ether);
-        assertEq(token.balanceOf(address(treasury)), 1000 ether);
-        treasury.transferERC20(address(token), address(0xfab), 1000 ether);
+    function testFail_modify_parameters_uint_unauthed() public removeAuth {
+        treasury.modifyParameters("epochLength", 1 weeks);
     }
 
-    function testTransferEther() external {
-        address(treasury).transfer(100 ether);
-        assertEq(address(treasury).balance, 100 ether);
-        treasury.transferEther(address(0xfab), 100 ether);
-        assertEq(address(treasury).balance, 0);
-        assertEq(address(0xfab).balance, 100 ether);
+    function testFail_modify_parameters_invalid_epochLength() public {
+        treasury.modifyParameters("epochLength", 0);
     }
 
-    function testFailTransferEtherUnauthorized() external removeAuth {
-        address(treasury).transfer(100 ether);
-        assertEq(address(treasury).balance, 100 ether);
-        treasury.transferEther(address(0xfab), 100 ether);
+    function test_modify_parameters_delegateAllowance_lower() public {
+        treasury.modifyParameters("delegateAllowance", 1 ether);
+        assertEq(treasury.delegateAllowance(), 1 ether);
+        assertEq(treasury.delegateLeftoverToSpend(), 1 ether);
     }
 
-    function testExternalCall() external {
-        Target target = new Target();
-        assertEq(target.calls(), 0);
-        address(treasury).transfer(100 ether);
-
-        treasury.externalCall(
-            address(target),
-            100 ether,
-            abi.encodePacked(Target.arbitraryCall.selector)
-        );
-        assertEq(target.calls(), 1);
+    function test_modify_parameters_delegateAllowance_higher() public {
+        treasury.modifyParameters("delegateAllowance", 1000 ether);
+        assertEq(treasury.delegateAllowance(), 1000 ether);
+        assertEq(treasury.delegateLeftoverToSpend(), allowance);
     }
 
-    function testFailExternalCallUnauthorized() external removeAuth {
-        Target target = new Target();
-        assertEq(target.calls(), 0);
-        address(treasury).transfer(100 ether);
+    function test_modify_parameters_delegate() public {
+        treasury.modifyParameters("treasuryDelegate", address(0x1));
+        assertEq(treasury.treasuryDelegate(), address(0x1));
+    }
 
-        treasury.externalCall(
-            address(target),
-            100 ether,
-            abi.encodePacked(Target.arbitraryCall.selector)
-        );
+    function testFail_modify_parameters_address_unauthed() public removeAuth {
+        treasury.modifyParameters("treasuryDelegate", address(0x1));
+    }
+
+    function test_transfer_ERC20_admin() external {
+        uint transferAmount = allowance * 2;
+        treasury.transferERC20(address(0xfab), transferAmount);
+        assertEq(token.balanceOf(address(treasury)), initialTreasuryBalance - transferAmount);
+        assertEq(token.balanceOf(address(0xfab)), transferAmount);
+    }
+
+    function test_transfer_ERC20_delegate() external {
+        uint transferAmount = allowance;
+        delegate.transferERC20(treasury, address(0xfab), transferAmount);
+        assertEq(token.balanceOf(address(treasury)), initialTreasuryBalance - transferAmount);
+        assertEq(treasury.delegateLeftoverToSpend(), 0);
+        assertEq(token.balanceOf(address(0xfab)), transferAmount);
+    }
+
+    function testFail_transfer_ERC20_delegate_over_allowance() external {
+        uint transferAmount = allowance + 1;
+        delegate.transferERC20(treasury, address(0xfab), transferAmount);
+    }
+
+    function test_transfer_ERC20_delegate_multiple_epochs() external {
+        uint transferAmount = allowance;
+        for (uint i; i < 10; i++) {
+            hevm.warp(now + epochLength + 1);
+            delegate.transferERC20(treasury, address(0xfab), transferAmount);
+            emit log_named_uint("epochStart", treasury.epochStart());
+        }
+
+        assertEq(token.balanceOf(address(treasury)), initialTreasuryBalance - transferAmount * 10);
+        assertEq(treasury.delegateLeftoverToSpend(), 0);
+        assertEq(token.balanceOf(address(0xfab)), transferAmount * 10);
+    }
+
+    function test_transfer_ERC20_delegate_multiple_epochs_no_spending_in_between() external {
+        uint transferAmount = allowance;
+        for (uint i; i < 10; i++) {
+            hevm.warp(now + 1 + epochLength * 3);
+            delegate.transferERC20(treasury, address(0xfab), transferAmount);
+            emit log_named_uint("epochStart", treasury.epochStart());
+        }
+
+        assertEq(token.balanceOf(address(treasury)), initialTreasuryBalance - transferAmount * 10);
+        assertEq(treasury.delegateLeftoverToSpend(), 0);
+        assertEq(token.balanceOf(address(0xfab)), transferAmount * 10);
+    }
+
+    function testFail_transfer_ERC20_unauthed() external removeAuth {
+        uint transferAmount = 1;
+        treasury.transferERC20(address(0xfab), transferAmount);
     }
 }
